b8a00cce19f8f2bd1ffd96ba495d59a8e020f5f3
root <sabrinalua@users.noreply.github.com>
2018-09-26 07:30:59 +0800

    docker installation script

---
+++ centos7/install_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum install yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+systemctl start docker
+systemctl enable docker
